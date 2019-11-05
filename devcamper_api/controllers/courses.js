db5396caf725442ef64038e4239da4cd1d7012b7
D3stros <patrick.fallmann@hotmail.com>
2019-11-05 14:58:29 +0100

    Update & Delete Course

--- devcamper_api/controllers/courses.js
+++ devcamper_api/controllers/courses.js
@@ -56,7 +56,7 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 exports.addCourse = asyncHandler(async (req, res, next) => {
 	req.body.bootcamp = req.params.bootcampId;
 
-	const bootcamp = await Course.findById(req.params.bootcampId);
+	const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 
 	if (!bootcamp) {
 		return next(
@@ -70,5 +70,50 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
 	res.status(200).json({
 		success: true,
 		data: course
+	});
+});
+
+// @desc      Update course
+// @route     PUT /api/v1/courses/:id
+// @access    Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+	let course = await Course.findById(req.params.id);
+
+	if (!course) {
+		return next(
+			new ErrorResponse(`No course with the id of ${req.params.id}`),
+			404
+		);
+	}
+
+	course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+		runValidators: true
+	});
+
+	res.status(200).json({
+		success: true,
+		data: course
+	});
+});
+
+// @desc      Delete course
+// @route     DELETE /api/v1/courses/:id
+// @access    Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+	const course = await Course.findById(req.params.id);
+
+	if (!course) {
+		return next(
+			new ErrorResponse(`No course with the id of ${req.params.id}`),
+			404
+		);
+	}
+
+	await course.remove();
+
+	res.status(200).json({
+		success: true,
+		data: {}
 	});
 });
